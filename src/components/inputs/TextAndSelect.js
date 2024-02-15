71d448844d996e1d9cdd6e34c5b27b952d7f21df
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2024-02-15 15:15:39 +0100

    Remove unnecessary imports

--- src/components/inputs/TextAndSelect.js
+++ src/components/inputs/TextAndSelect.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { createUseStyles } from 'react-jss';
 import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
